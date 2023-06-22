189a8cabe58c3f079b984cd0b7a6163f38f81b41
G-Ryoto <126740512+G-Ryoto@users.noreply.github.com>
2023-06-22 20:54:47 +0900

    Revert "商品情報編集機能"

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show ]
-  before_action :set_item, only: [:edit,:update,:show ]
+  before_action :authenticate_user!, except: [:create,:index,:show ]
   def index
     @item = Item.order("created_at DESC")
     #@items = Item.find(1)
@@ -22,31 +21,15 @@ class ItemsController < ApplicationController
   end 
 
   def show
+    @item = Item.find(params[:id])
+    
   end 
   
-  def edit
-    if current_user != @item.user
-      redirect_to root_path
-    end
-  end
 
-  def update
-    if @item.update(item_params)
-      redirect_to item_path
-    else
-      render :edit
-    end
-  end
   
   private
 
   def item_params
     params.require(:item).permit(:image, :product_explanation, :product_name, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
-
-  def set_item
-    @item = Item.find(params[:id])
-
-  end
-
 end
