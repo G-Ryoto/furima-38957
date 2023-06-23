6ace44bf49bb0996e3fc1157249ef81f398f0ac9
G-Ryoto <ryoto150627@gmail.com>
2023-06-23 21:22:50 +0900

    商品削除機能実装修正❹

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-     if current_user == @item.seller
+     if current_user == @item.user
       @item.destroy
       redirect_to root_path
     else
