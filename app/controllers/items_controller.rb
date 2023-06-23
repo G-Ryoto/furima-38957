0abb6e6babd097eff3455766b47c69e8ae56ef32
G-Ryoto <ryoto150627@gmail.com>
2023-06-23 19:31:51 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show,:destroy ]
+  before_action :authenticate_user!, except: [:index,:show, ]
   before_action :set_item, only: [:edit,:update,:show,:destroy ]
   def index
     @item = Item.order("created_at DESC")
-    #@items = Item.find(1)
-    #@images = Image.all
-
+  
   end 
 
   def new
@@ -40,7 +38,10 @@ class ItemsController < ApplicationController
 
   def destroy
      @item.destroy
-     redirect_to root_path
+     if current_user == @item.seller
+      @item.destroy
+      redirect_to root_path, 
+     end
   end
   
   private
